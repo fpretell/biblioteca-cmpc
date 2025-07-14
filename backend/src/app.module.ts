dbb4697bb0ef3d7b57d5e5b60476fdff2d17c36a
Felix Pretell <fpretellmdz@gmail.com>
2025-07-13 21:37:14 -0300

    Docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,32 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Test } from './domains/test.models';
-// import { TestModule } from './test/test.module';
-import { TestModule } from './domains/test.module.ts';
+import { ConfigModule } from '@nestjs/config';
+import { LibrosModule } from './domains/libros/libros.module';
+import { GenerosModule } from './domains/generos/generos.module';
+import { EditorialesModule } from './domains/editoriales/editoriales.module';
+import { AutoresModule } from './domains/autores/autores.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
+      port: +(process.env.DB_PORT || 5432),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      models: [Test],
       autoLoadModels: true,
-      synchronize: false, // usamos init.sql
+      synchronize: true,
+      logging: false,
+      models: [], // ← luego agregamos modelos acá o usamos autoLoadModels
     }),
-    TestModule,
+    LibrosModule,
+    GenerosModule,
+    EditorialesModule,
+    AutoresModule,
+    AuthModule
   ],
 })
 export class AppModule {}
