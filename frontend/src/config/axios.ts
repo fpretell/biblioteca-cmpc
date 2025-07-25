d496bdfbf333589f86aee08f58e95b410355dd15
Felix Pretell <fpretellmdz@gmail.com>
2025-07-25 01:35:24 -0300

    Fix axios

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000/api/v1',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api/v1',
 });
 
 instance.interceptors.request.use(config => {
